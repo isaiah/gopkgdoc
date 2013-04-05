db09d6c6b2bc09a9094297540ec52a64305fd6bb
Isaiah Peng <issaria@gmail.com>
2013-04-06 00:57:07 +0200

    import examples, fix import path links

--- gddo-server/godocs.go
+++ gddo-server/godocs.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"bytes"
-    "net"
 	"database/sql"
 	"fmt"
+	"net"
 	//"github.com/garyburd/gopkgdoc/database"
 	"github.com/garyburd/gopkgdoc/doc"
-    htemp "html/template"
 	_ "github.com/lib/pq"
 	godoc "go/doc"
+	htemp "html/template"
 	//"net/http"
 	"net/url"
 	"regexp"
@@ -17,15 +17,17 @@ import (
 )
 
 type timeoutConn struct {
-  net.Conn
+	net.Conn
 }
+
 var dialTimeout = 30 * time.Second
+
 func timeoutDial(network, addr string) (net.Conn, error) {
-    c, err := net.DialTimeout(network, addr, dialTimeout)
-    if err != nil {
-        return nil, err
-    }
-    return &timeoutConn{Conn: c}, nil
+	c, err := net.DialTimeout(network, addr, dialTimeout)
+	if err != nil {
+		return nil, err
+	}
+	return &timeoutConn{Conn: c}, nil
 }
 
 func main() {
@@ -40,65 +42,80 @@ func main() {
 	check(err)
 	_, err = pg.Exec("delete from namespaces where version = '1.1'")
 	check(err)
-    root := "/home/isaiah/codes/go/go/src/pkg/"
-    pkgs, err := doc.GetLocalDoc(root)
-    fmt.Println(len(pkgs))
-    for _, pkg := range pkgs {
-        fmt.Print(pkg.ImportPath)
-        //fmt.Println(len(pkg.Funcs))
-
-        //fmt.Println(pkg)
-        store(pkg, pg)
-    }
+	root := "/home/isaiah/codes/go/go/src/pkg/"
+	pkgs, err := doc.GetLocalDoc(root)
+	for _, pkg := range pkgs {
+                fmt.Println(pkg.ImportPath)
+		store(pkg, pg)
+	}
 }
 
 func store(pkg *doc.Package, pg *sql.DB) {
-      version := "1.1"
-      nsSql, err := pg.Prepare("insert into namespaces (name, doc, version, library_id) values ($1, $2, " + version + ", 2) RETURNING id")
-      check(err)
-      funSql, err := pg.Prepare("insert into functions (name, doc, arglists_comp, version, url_friendly_name, functional_id, functional_type) values ($1, $2, $3, " + version + ", $4, $5, $6)")
-      check(err)
-      typeSql, err := pg.Prepare("insert into type_classes (name, doc, arglists_comp, type, namespace_id, version, created_at, updated_at) values ($1, $2, $3, 'StructType', $4, " + version + ", $5, $6) RETURNING id")
-      check(err)
-      var nsId int
-      err = nsSql.QueryRow(pkg.ImportPath, comment(pkg.Doc)).Scan(&nsId)
-      if err != nil {
-          panic(err)
-      }
-      now := time.Now()
+	version := "1.1"
+	nsSql, err := pg.Prepare("insert into namespaces (name, doc, version, library_id) values ($1, $2, " + version + ", 2) RETURNING id")
+	check(err)
+	funSql, err := pg.Prepare("insert into functions (name, doc, arglists_comp, version, url_friendly_name, functional_id, functional_type, shortdoc) values ($1, $2, $3, " + version + ", $4, $5, $6, $7) RETURNING id")
+	check(err)
+	typeSql, err := pg.Prepare("insert into type_classes (name, doc, arglists_comp, type, namespace_id, version, created_at, updated_at, shortdoc) values ($1, $2, $3, 'StructType', $4, " + version + ", $5, $6, $7) RETURNING id")
+	check(err)
+	exampleSql, err := pg.Prepare("INSERT INTO examples (name, body, doc, output, examplable_id, examplable_type) VALUES ($1, $2, $3, $4, $5, $6)")
+	check(err)
+        rootPath := "/gopkg/" + version
+	var nsId int
+        pkgPath := pkg.ImportPath
+	err = nsSql.QueryRow(pkgPath, comment(pkg.Doc)).Scan(&nsId)
+	if err != nil {
+		panic(err)
+	}
+	now := time.Now()
 
-      for _, fun := range pkg.Funcs {
-          _, err = funSql.Exec(fun.Name, comment(fun.Doc), codeFn(fun.Decl), fun.Name, nsId, "Namespace")
-          if err != nil {
-              panic(err)
-          }
-      }
+	for _, fun := range pkg.Funcs {
+                var funId int
+                err = funSql.QueryRow(fun.Name, comment(fun.Doc), codeFn(rootPath, pkgPath, fun.Decl), fun.Name, nsId, "Namespace", fun.Decl.Text).Scan(&funId)
+		if err != nil {
+			panic(err)
+		}
+		for _, eg := range fun.Examples {
+			_, err = exampleSql.Exec(eg.Name, eg.Code.Text, comment(eg.Doc), eg.Output, funId, "Function")
+			check(err)
+		}
+	}
 
-      for _, t := range pkg.Types {
-          var id int
-          err = typeSql.QueryRow(t.Name, comment(t.Doc), codeFn(t.Decl), nsId, now, now).Scan(&id)
-          check(err)
-          for _, fun := range t.Funcs {
-              _, err = funSql.Exec(fun.Name, comment(fun.Doc), codeFn(fun.Decl), fun.Name, id, "TypeClass")
-              if err != nil {
-                  panic(err)
-              }
-          }
+	for _, t := range pkg.Types {
+		var id int
+		err = typeSql.QueryRow(t.Name, comment(t.Doc), codeFn(rootPath, pkgPath, t.Decl), nsId, now, now, t.Decl.Text).Scan(&id)
+		check(err)
+		for _, fun := range t.Funcs {
+                        var funId int
+			err = funSql.QueryRow(fun.Name, comment(fun.Doc), codeFn(rootPath, pkgPath, fun.Decl), fun.Name, id, "TypeClass", fun.Decl.Text).Scan(&funId)
+			if err != nil {
+				panic(err)
+			}
 
-          for _, fun := range t.Methods {
-              _, err = funSql.Exec(fun.Name, comment(fun.Doc), codeFn(fun.Decl), fun.Name, id, "TypeClass")
-              if err != nil {
-                  panic(err)
-              }
-          }
+			for _, eg := range fun.Examples {
+				_, err = exampleSql.Exec(eg.Name, eg.Code.Text, comment(eg.Doc), eg.Output, funId, "Function")
+				check(err)
+			}
+		}
 
-          //for _, eg := range t.Examples {
-          //        fmt.Println(eg.Name)
-          //        fmt.Println(eg.Code.Text)
-          //        fmt.Println(eg.Doc)
-          //        fmt.Println(eg.Output)
-          //}
-      }
+		for _, fun := range t.Methods {
+                        var funId int
+			err = funSql.QueryRow(fun.Name, comment(fun.Doc), codeFn(rootPath, pkgPath, fun.Decl), fun.Name, id, "TypeClass", fun.Decl.Text).Scan(&funId)
+			if err != nil {
+				panic(err)
+			}
+
+			for _, eg := range fun.Examples {
+				_, err = exampleSql.Exec(eg.Name, eg.Code.Text, comment(eg.Doc), eg.Output, funId, "Function")
+				check(err)
+			}
+		}
+
+		for _, eg := range t.Examples {
+			_, err = exampleSql.Exec(eg.Name, eg.Code.Text, comment(eg.Doc), eg.Output, id, "TypeClass")
+			check(err)
+		}
+	}
 }
 
 var (
@@ -133,7 +150,7 @@ func check(err error) {
 		panic(err)
 	}
 }
-func codeFn(c doc.Code) string {
+func codeFn(rootPath string, pkg string, c doc.Code) string {
 	var buf bytes.Buffer
 	last := 0
 	src := []byte(c.Text)
@@ -141,7 +158,7 @@ func codeFn(c doc.Code) string {
 		htemp.HTMLEscape(&buf, src[last:a.Pos])
 		switch a.Kind {
 		case doc.PackageLinkAnnotation:
-			p := "/" + a.ImportPath
+			p := rootPath + "/" + a.ImportPath
 			buf.WriteString(`<a href="`)
 			buf.WriteString(escapePath(p))
 			buf.WriteString(`">`)
@@ -150,15 +167,17 @@ func codeFn(c doc.Code) string {
 		case doc.ExportLinkAnnotation, doc.BuiltinAnnotation:
 			p := a.ImportPath
 			if a.Kind == doc.BuiltinAnnotation {
-				p = "/builtin"
+				p = rootPath + "/builtin"
 			} else if p != "" {
-				p = "/" + p
-			}
+				p = rootPath + "/" + p
+			} else {
+                                p = rootPath + "/" + pkg
+                        }
 			n := src[a.Pos:a.End]
 			n = n[bytes.LastIndex(n, period)+1:]
 			buf.WriteString(`<a href="`)
 			buf.WriteString(escapePath(p))
-			buf.WriteByte('#')
+			buf.WriteByte('/')
 			buf.WriteString(escapePath(string(n)))
 			buf.WriteString(`">`)
 			htemp.HTMLEscape(&buf, src[a.Pos:a.End])
@@ -188,4 +207,3 @@ func escapePath(s string) string {
 }
 
 var period = []byte{'.'}
-
